6267429d3bff7499a9741d17a06aeaf4dc0bdc30
JanDoe91 <jan.philipp.zautke@gmx.de>
2015-03-23 11:35:52 +0100

    Aufgabe zur RabbitMQ durchgeführt

--- OpcProjekt/src/main/java/OPCTest/Types/Type.java
+++ OpcProjekt/src/main/java/OPCTest/Types/Type.java
@@ -2,6 +2,7 @@ package OPCTest.Types;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -10,12 +11,17 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.transform.Result;
 
 import org.opcfoundation.ua.builtintypes.DateTime;
 import org.opcfoundation.ua.builtintypes.NodeId;
 import org.opcfoundation.ua.builtintypes.StatusCode;
 import org.opcfoundation.ua.builtintypes.UnsignedShort;
 import org.opcfoundation.ua.builtintypes.Variant;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
 
 //@XmlType(name="",propOrder={
 //		"nodeId",
@@ -26,81 +32,154 @@ import org.opcfoundation.ua.builtintypes.Variant;
 //		"serverTimestamp",
 //		"serverPicoseconds"
 //})
-@XmlRootElement//(name = "Type")
+@XmlRootElement
+// (name = "Type")
 public abstract class Type {
-	
-	
 
-	
-	@XmlElement//(required=true)
+	private final static String host = "localhost";
+	private final static String QUEUE_NAME = "ProSys_OPC";
+
+	@XmlElement
+	// (required=true)
 	private int statusCode;
-	
-	@XmlElement//(required=true)
+
+	@XmlElement
+	// (required=true)
 	private long sourceTimestamp;
-	
-	@XmlElement//(required=true)
+
+	@XmlElement
+	// (required=true)
 	private int sourcePicoseconds;
-	
-	@XmlElement//(required=true)
+
+	@XmlElement
+	// (required=true)
 	private long serverTimestamp;
-	
-	@XmlElement//(required=true)
+
+	@XmlElement
+	// (required=true)
 	private int serverPicoseconds;
-	
-	@XmlElement//(required=true)
-	private String value;
-	
-	@XmlElement//(required=true)
+
+	@XmlElement
+	// (required=true)
 	private String bezeichnung;
-	
-	public Type(){
-		
+
+	public Type() {
+
 	}
-	public Type (String bezeichnung, Variant value, StatusCode statusCode,
+
+	public Type(String bezeichnung, StatusCode statusCode,
 			DateTime sourceTimestamp, UnsignedShort sourcePicoseconds,
 			DateTime serverTimestamp, UnsignedShort serverPicoseconds) {
 		this.statusCode = statusCode.getValueAsIntBits();
 		this.sourceTimestamp = sourceTimestamp.getValue();
 		this.sourcePicoseconds = sourcePicoseconds.getValue();
-		if(serverTimestamp != null){
+		if (serverTimestamp != null) {
 			this.serverTimestamp = serverTimestamp.getValue();
 		}
-		
+
 		this.serverPicoseconds = serverPicoseconds.getValue();
-		this.value = "" +  value.getValue();
-		this.bezeichnung	= bezeichnung;
+		this.bezeichnung = bezeichnung;
 	}
-	
-	public void writeXML(){
+
+	public void writeXML() {
 
 		try {
 			// JAXB initialisieren
-			JAXBContext context;
-			context = JAXBContext.newInstance(Type.class);
-			Marshaller jaxbMarshaller = context.createMarshaller();
-			Unmarshaller jaxbUnmarshaller = context.createUnmarshaller();
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			
-			// Datei erzeugen 
-			File f = File.createTempFile("OpcInt",".xml");
-			
-			// Objekt in tmp-Datei schreiben
-			jaxbMarshaller.marshal(this, f);
-			jaxbMarshaller.marshal(this, System.out);
+			JAXBContext context = null;
+			switch (this.bezeichnung) {
+			case "Counter1":
+				context = JAXBContext.newInstance(OpcInt.class);
+				break;
+			case "Square1":
+				context = JAXBContext.newInstance(OpcDouble.class);
+				break;
+			case "Expression1":
+				context = JAXBContext.newInstance(OpcDouble.class);
+				break;
+			case "Random1":
+				context = JAXBContext.newInstance(OpcDouble.class);
+				break;
+			case "Sinusoid1":
+				context = JAXBContext.newInstance(OpcDouble.class);
+				break;
+			case "Sawtooth1":
+				context = JAXBContext.newInstance(OpcDouble.class);
+				break;
+			case "Triangle1":
+				context = JAXBContext.newInstance(OpcDouble.class);
+				break;
+			default:
+				context = null;
+			}
+			if (context == null) {
+				System.out.println("Context ist Null. Klasse Type. Methode writeXML");
+			} else {
+				Marshaller jaxbMarshaller = context.createMarshaller();
+				Unmarshaller jaxbUnmarshaller = context.createUnmarshaller();
+				jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,
+						true);
 
-			
-			System.out.println("XML geschrieben in Datei: " + f.toString());
+				// Datei muss nicht mehr erzeugt werden
+				// File f = File.createTempFile(this.bezeichnung ,".xml");
+				StringWriter sw = new StringWriter();
+
+				// tmp Datei muss nicht mehr erzeugt werden
+				// Objekt in tmp-Datei schreiben
+				// jaxbMarshaller.marshal(this, f);
+
+				// Marschal into Stringwriter
+				jaxbMarshaller.marshal(this, sw);
+				// Get String from StrinWriter
+				String message = sw.toString();
+				// Print the Message
+				System.out.println(message);
+
+				createQueue(message);
+				// System.out.println("XML geschrieben in Datei: " +
+				// f.toString());
+			}
 		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
+			System.out.println("XML konnte nicht generiert werden");
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Fehler beim erzeugen der Datei");
 		}
 
 	}
 
-	
-	
+	private static void createQueue(String message) {
+		Connection connection = null;
+		Channel channel = null;
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("localhost");
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+			System.out.println(" [x] Sent '" + message + "'");
+			channel.close();
+			connection.close();
+		} catch (Exception e) {
+			System.out.println("Schreiben in die Queue nicht möglich");
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (Exception f) {
+					System.out
+							.println("Connection konnte nicht geschlossen werden");
+					f.printStackTrace();
+				}
+			}// ifconnection
+			if (channel != null) {
+				try {
+					channel.close();
+				} catch (Exception f) {
+					System.out
+							.println("Channel konnte nicht geschlossen werden");
+					f.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
